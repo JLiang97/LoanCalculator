e536e0c93dde2b06a53c4cdaabc2cb01fce52650
JLiang <lowjianliang@gmail.com>
2017-10-31 10:15:17 +0800

    hi

---
+++ app/src/main/java/com/example/user/loancalculator/MainActivity.java
@@ -0,0 +1,27 @@
+package com.example.user.loancalculator;
+
+import android.content.Intent;
+import android.support.v4.app.ShareCompat;
+import android.support.v4.media.MediaBrowserServiceCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    public static final String LOAN_STATUS = "status";
+    public static final String LOAN_REPAYMENT = "repayment";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void calculateRepayment(View view){
+        Intent intent = new Intent(this, ResultActivity.class);
+
+        intent.putExtra(LOAN_STATUS, "Approve");
+        startActivity(intent);
+    }
+}
---
+++ app/src/main/java/com/example/user/loancalculator/ResultActivity.java
@@ -0,0 +1,23 @@
+package com.example.user.loancalculator;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class ResultActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_result);
+
+        Intent intent = getIntent();
+        String status = intent.getStringExtra(MainActivity.LOAN_STATUS);
+        double repayment = intent.getDoubleExtra(MainActivity.LOAN_REPAYMENT, 0);
+    }
+
+    public void closeActivity(View view){
+    finish();
+    }
+}
